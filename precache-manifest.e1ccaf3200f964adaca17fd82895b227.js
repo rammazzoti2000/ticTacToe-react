79e1225206690eff164620e7e832492e78e65d16
Alexandru <rammazzoti2000@gmail.com>
2020-08-10 16:27:43 +0200

    Updates

---
+++ precache-manifest.e1ccaf3200f964adaca17fd82895b227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1497e3e9d3ce8bf6a36617afd8beae5",
+    "url": "/ticTacToe-react/index.html"
+  },
+  {
+    "revision": "e70ebb311fa1e901b98f",
+    "url": "/ticTacToe-react/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "c5fde3cdf75186bb9ccc",
+    "url": "/ticTacToe-react/static/js/2.e6f23ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe-react/static/js/2.e6f23ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70ebb311fa1e901b98f",
+    "url": "/ticTacToe-react/static/js/main.b399656f.chunk.js"
+  },
+  {
+    "revision": "973eff928ac78e179f9a",
+    "url": "/ticTacToe-react/static/js/runtime-main.b7268f58.js"
+  }
+]);
